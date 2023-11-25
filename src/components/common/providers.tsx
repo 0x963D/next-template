35fab0291fbe35975d6f6e60d3c08438d36825a2
ToDy95 <todyimmortal@gmail.com>
2023-11-25 19:19:16 +0200

    redux-toolkit

--- src/components/common/providers.tsx
+++ src/components/common/providers.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import type { ReactNode } from "react"
+import { store } from "@/store/store"
 import { SessionProvider } from "next-auth/react"
+import { Provider } from "react-redux"
 
 import Sentry from "@/lib/sentry" // needs to be imported first
 
@@ -9,13 +11,15 @@ import { ThemeProvider } from "@/components/common/theme-provider"
 
 const AppProviders = ({ children }: { children: ReactNode | ReactNode[] }): React.JSX.Element => {
   return (
-    <Sentry.ErrorBoundary showDialog>
-      <SessionProvider>
-        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-          {children}
-        </ThemeProvider>
-      </SessionProvider>
-    </Sentry.ErrorBoundary>
+    <Provider store={store}>
+      <Sentry.ErrorBoundary showDialog>
+        <SessionProvider>
+          <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+            {children}
+          </ThemeProvider>
+        </SessionProvider>
+      </Sentry.ErrorBoundary>
+    </Provider>
   )
 }
 
